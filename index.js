c656a887166a61e534db5df3b3bf212f74b37370
chang <cshin7374@gmail.com>
2022-05-22 22:05:28 -0500

    commit tree 7c2cb79780c67fc9c72a8ee3577aa04302eafca4

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(arr, driver) {
+    return arr.filter(name => name.toUpperCase() === driver.toUpperCase())
+}
+
+function fuzzyMatch(arr, driver) {
+    return arr.filter(name => name.substring(0,driver.length) === driver)
+}
+
+function matchName (arr, driver) {
+    return arr.filter(item => item.name === driver)
+}
